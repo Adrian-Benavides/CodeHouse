7710e422f4250f37c608eb756d895ae2523c1a8a
Adrian-Benavides <61027259+Adrian-Benavides@users.noreply.github.com>
2023-11-12 19:35:31 -0300

    Update ticketDTO.js

--- Entregables/Entregable_12/src/dto/ticketDTO.js
+++ Entregables/Entregable_12/src/dto/ticketDTO.js
@@ -1,18 +1,11 @@
 export default class TicketDTO{
     #ticket;
-    constructor(data){
-        //console.log(puser);
-        this.#ticket = {           
-            first_name: puser.first_name,
-            last_name: puser.last_name,
-            email: puser.email,
-            age: puser.age,
-            role : puser.role
-        }
+    constructor(){
+        
         
     }
     getUser(){
-        return this.#ticket;
+        return null;
     }
 
 }
